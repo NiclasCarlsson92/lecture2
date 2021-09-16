3c92a92e8cb76869afdc1e58ca592bae95a88418
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2021-09-16 13:56:41 +0200

    Some code changed

--- src/lecture2/Main.java
+++ src/lecture2/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        System.out.println("We gotta fix this problem");
+
+        // Not necessary..
+        int i = 0;
+        int j = 1;
     }
 }
